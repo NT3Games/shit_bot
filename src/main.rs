687d51f07554510d8d39d6d9aca1c2bc32d294ce
71e6fd52 <71e6fd52@gmail.com>
2022-05-22 14:48:08 +0800

    pull

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
+use anyhow::Result;
+use redis::AsyncCommands;
 use teloxide::{
     prelude::*,
     types::{ChatId, UserId},
     utils::command::BotCommands,
     RequestError,
 };
-use tokio::sync::{Mutex, OnceCell};
+use tokio::sync::OnceCell;
 
 const SHIT_HILL: ChatId = ChatId(0 /*CLEANED*/);
 const SOURCE: ChatId = ChatId(0 /*CLEANED*/);
 const NT3: UserId = UserId(0 /*CLEANED*/);
 
-static LAST_MESSAGE: OnceCell<Mutex<Option<i32>>> = OnceCell::const_new();
-
-async fn last_message() -> &'static Mutex<Option<i32>> {
-    LAST_MESSAGE
-        .get_or_init(|| async { Mutex::new(None) })
+static CLIENT: OnceCell<redis::Client> = OnceCell::const_new();
+async fn get_client() -> &'static redis::Client {
+    CLIENT
+        .get_or_init(|| async { redis::Client::open("unix:///run/redis/redis.sock").unwrap() })
         .await
 }
+
+const LAST_SENT_KEY: &str = "_shit_bot_last_send_message";
+const LAST_SHIT_KEY: &str = "_shit_bot_last_shit_message";
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     log::info!("Starting command bot...");
 
-    let bot = Bot::from_env().auto_send();
+    let bot = Bot::from_env();
 
     let handler = Update::filter_message()
         .branch(
@@ -72,16 +76,20 @@ enum Command {
     Shit,
     #[command(description = "查看源代码")]
     Source,
+    #[command(description = "拉最后的屎")]
+    Pull,
 }
 
-async fn command_handle(
-    bot: AutoSend<Bot>,
-    message: Message,
-    command: Command,
-) -> Result<(), RequestError> {
+async fn command_handle(bot: Bot, message: Message, command: Command) -> Result<()> {
     match command {
         Command::Help => {
             bot.send_message(message.chat.id, Command::descriptions().to_string())
+                .send()
+                .await?;
+        }
+        Command::Source => {
+            bot.send_message(message.chat.id, "https://gitlab.com/71e6fd52/shit_bot")
+                .send()
                 .await?;
         }
         Command::Shit => {
@@ -89,20 +97,22 @@ async fn command_handle(
                 return Ok(());
             }
             if message.chat.id != SOURCE {
-                bot.send_message(message.chat.id, "机器人不允许在此处使用")
-                    .await?;
+                let mut request = bot.send_message(message.chat.id, "机器人不允许在此处使用");
+                request.reply_to_message_id = Some(message.id);
+                request.send().await?;
                 return Ok(());
             };
             let chat_member = bot
                 .get_chat_member(SHIT_HILL, message.from().unwrap().id)
+                .send()
                 .await;
             if let Err(RequestError::Api(teloxide::ApiError::UserNotFound)) = chat_member {
-                let mut request = bot.inner().send_message(
+                let mut request = bot.send_message(
                     message.chat.id,
                     "请先加入 https://t.me/nipple_hill 以使用此命令",
                 );
                 request.reply_to_message_id = Some(message.id);
-                request.send().await?;
+                replace_send(bot, request).await?;
                 return Ok(());
             } else {
                 chat_member?;
@@ -111,37 +121,76 @@ async fn command_handle(
             if let Some(reply) = message.reply_to_message() {
                 forward_shit(bot, reply.to_owned()).await?;
             } else {
-                let mut request = bot.inner().send_message(message.chat.id, "没有选择消息");
+                let mut request = bot.send_message(message.chat.id, "没有选择消息");
                 request.reply_to_message_id = Some(message.id);
-                request.send().await?;
+                replace_send(bot, request).await?;
             };
         }
-        Command::Source => {
-            bot.send_message(message.chat.id, "https://gitlab.com/71e6fd52/shit_bot")
-                .await?;
+        Command::Pull => {
+            if message.chat.id != SOURCE {
+                let mut request = bot.send_message(message.chat.id, "机器人不允许在此处使用");
+                request.reply_to_message_id = Some(message.id);
+                request.send().await?;
+                return Ok(());
+            };
+
+            let id: Option<i32> = {
+                let mut con = get_client().await.get_async_connection().await?;
+                con.get(LAST_SHIT_KEY).await?
+            };
+            let text = if let Some(id) = id {
+                format!("https://t.me/nipple_hill/{}", id)
+            } else {
+                "未找到！".to_string()
+            };
+
+            let mut request = bot.send_message(message.chat.id, text);
+            request.reply_to_message_id = Some(message.id);
+            request.send().await?;
         }
     };
 
     Ok(())
 }
 
-async fn forward_shit(bot: AutoSend<Bot>, message: Message) -> Result<(), RequestError> {
+async fn forward_shit(bot: Bot, message: Message) -> Result<()> {
     let sent = bot
         .forward_message(SHIT_HILL, message.chat.id, message.id)
+        .send()
         .await?;
-    let mut request = bot.inner().send_message(
+
+    {
+        let mut con = get_client().await.get_async_connection().await?;
+        con.set(LAST_SHIT_KEY, sent.id).await?;
+    }
+
+    let mut request = bot.send_message(
         message.chat.id,
         format!("https://t.me/nipple_hill/{}", sent.id),
     );
     request.reply_to_message_id = Some(message.id);
     request.disable_web_page_preview = Some(true);
-    let res = request.send().await?;
-    {
-        let mut last = last_message().await.lock().await;
-        if let Some(id) = *last {
-            bot.delete_message(SOURCE, id).await?;
-        }
-        *last = Some(res.id);
+    replace_send(bot, request).await?;
+    // let res = request.send().await?;
+    Ok(())
+}
+
+async fn replace_send(
+    bot: Bot,
+    message: teloxide::requests::JsonRequest<teloxide::payloads::SendMessage>,
+) -> Result<()> {
+    use teloxide::types::Recipient::Id;
+
+    if message.chat_id != Id(SOURCE) {
+        panic!()
+    }
+    let res = message.send().await?;
+
+    let mut con = get_client().await.get_async_connection().await?;
+    let last: Option<i32> = con.get(LAST_SENT_KEY).await?;
+    con.set(LAST_SENT_KEY, res.id).await?;
+    if let Some(id) = last {
+        bot.delete_message(SOURCE, id).send().await?;
     }
     Ok(())
 }
